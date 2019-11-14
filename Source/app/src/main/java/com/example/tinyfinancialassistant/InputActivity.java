d84c8f4c2f3314acc828ab6c690f5ee5a944861d
Zhelin Li <zli251@ucsc.edu>
2019-11-13 20:24:36 -0800

    quick fix for crash

--- Source/app/src/main/java/com/example/tinyfinancialassistant/InputActivity.java
+++ Source/app/src/main/java/com/example/tinyfinancialassistant/InputActivity.java
@@ -6,6 +6,7 @@ import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
@@ -13,7 +14,7 @@ import android.widget.Toast;
 public class InputActivity extends AppCompatActivity {
     SQLiteDatabase mDB;
     EditText inputEditText, inputNote;
-    RadioButton inputFoodButton, inputTransportationButton, inputStudyButton, inputHousingButton,
+    Button inputFoodButton, inputTransportationButton, inputStudyButton, inputHousingButton,
             inputEntertainmentButton, inputShoppingButton, inputCleaningButton,
             inputPersonalCareButton, inputHobbyButton, inputOtherButton, inputAddItemButton;
     String type = "";
@@ -25,19 +26,98 @@ public class InputActivity extends AppCompatActivity {
 
         AllDBHelper dbHelper = new AllDBHelper(this);
         mDB = dbHelper.getWritableDatabase();
-
+        inputEditText = findViewById(R.id.inputEditText);
+        inputNote = findViewById(R.id.inputNote);
+        inputFoodButton = findViewById(R.id.inputFoodButton);
+        inputTransportationButton = findViewById(R.id.inputTransportationButton);
+        inputStudyButton = findViewById(R.id.inputStudyButton);
+        inputHousingButton = findViewById(R.id.inputHousingButton);
+        inputEntertainmentButton = findViewById(R.id.inputEntertainmentButton);
+        inputShoppingButton = findViewById(R.id.inputShoppingButton);
+        inputCleaningButton = findViewById(R.id.inputCleaningButton);
+        inputPersonalCareButton = findViewById(R.id.inputPersonalCareButton);
+        inputHobbyButton = findViewById(R.id.inputHobbyButton);
+        inputOtherButton = findViewById(R.id.inputOtherButton);
+        inputAddItemButton = findViewById(R.id.inputAddItemButton);
+        inputFoodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeFood();
+            }
+        });
+        inputTransportationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeTransportation();
+            }
+        });
+        inputStudyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeStudy();
+            }
+        });
+        inputHousingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeHousing();
+            }
+        });
+        inputEntertainmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeEntertainment();
+            }
+        });
+        inputShoppingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeShopping();
+            }
+        });
+        inputCleaningButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeCleaning();
+            }
+        });
+        inputPersonalCareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypePersonalCare();
+            }
+        });
+        inputHobbyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeHobby();
+            }
+        });
+        inputOtherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setTypeOther();
+            }
+        });
+        inputAddItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addItem();
+            }
+        });
     }
-    public void setTypeFood(View view) {type = "Food";}
-    public void setTypeTransportation(View view) {type = "Transportation";}
-    public void setTypeStudy(View view) {type = "Study";}
-    public void setTypeHousing(View view) {type = "Housing";}
-    public void setTypeEntertainment(View view) {type = "Entertainment";}
-    public void setTypeShopping(View view) {type = "Shopping";}
-    public void setTypeCleaning(View view) {type = "Cleaning";}
-    public void setTypePersonalCare(View view) {type = "PersonalCare";}
-    public void setTypeHobby(View view) {type = "Hobby";}
-    public void setTypeOther(View view) {type = "Other";}
-    public void addItem(View view) {
+
+    private void setTypeFood() {type = "Food";}
+    private void setTypeTransportation() {type = "Transportation";}
+    private void setTypeStudy() {type = "Study";}
+    private void setTypeHousing() {type = "Housing";}
+    private void setTypeEntertainment() {type = "Entertainment";}
+    private void setTypeShopping() {type = "Shopping";}
+    private void setTypeCleaning() {type = "Cleaning";}
+    private void setTypePersonalCare() {type = "PersonalCare";}
+    private void setTypeHobby() {type = "Hobby";}
+    private void setTypeOther() {type = "Other";}
+    private void addItem() {
         if (inputEditText.getText().toString().trim().length() == 0 || type == "" || type == null) {
             Toast.makeText(this, "Please input value or select type", Toast.LENGTH_SHORT).show();
         }
@@ -52,6 +132,7 @@ public class InputActivity extends AppCompatActivity {
             mDB.insert(AllContract.AllEntry.TABLE_NAME, null, cv);
             Toast.makeText(this, "Data Saved", Toast.LENGTH_SHORT).show();
             inputEditText.getText().clear();
+            inputNote.getText().clear();
         }
     }
 
