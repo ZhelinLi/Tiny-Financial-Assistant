84292382e1a4b50d283a388bfd55a3b652682e29
zhyang98 <49853737+zhyang98@users.noreply.github.com>
2019-11-24 23:47:27 -0800

    updated calendar

--- Source/app/src/main/java/com/example/tinyfinancialassistant/CalenderActivity.java
+++ Source/app/src/main/java/com/example/tinyfinancialassistant/CalenderActivity.java
@@ -1,14 +1,26 @@
 package com.example.tinyfinancialassistant;
 
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class CalenderActivity extends AppCompatActivity {
-
+    CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        calendarView = findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (month+1) + "/" + dayOfMonth + "/" + year;
+                Toast.makeText(CalenderActivity.this, date, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
