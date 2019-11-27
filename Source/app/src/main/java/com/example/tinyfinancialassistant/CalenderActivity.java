05b7ca6b6300178882306bed1d7a32bcfe0ace5b
zhyang98 <49853737+zhyang98@users.noreply.github.com>
2019-11-26 17:38:33 -0800

    working calendar

--- Source/app/src/main/java/com/example/tinyfinancialassistant/CalenderActivity.java
+++ Source/app/src/main/java/com/example/tinyfinancialassistant/CalenderActivity.java
@@ -1,19 +1,26 @@
 package com.example.tinyfinancialassistant;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.squareup.timessquare.CalendarPickerView;
+import com.savvi.rangedatepicker.CalendarPickerView;
+import com.savvi.rangedatepicker.SubTitle;
 
-import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
+import java.util.Locale;
 
 public class CalenderActivity extends AppCompatActivity {
     //CalendarView calendarView;
+    CalendarPickerView calendar;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,34 +34,59 @@ public class CalenderActivity extends AppCompatActivity {
                 Toast.makeText(CalenderActivity.this, date, Toast.LENGTH_SHORT).show();
             }
         });*/
-        Date today = new Date();
-        Calendar nextYear = Calendar.getInstance();
-        nextYear.add(Calendar.YEAR, 1);
+        final Calendar nextYear = Calendar.getInstance();
+        nextYear.add(Calendar.YEAR, 10);
 
-        CalendarPickerView datepicker = findViewById(R.id.calendar);
-        datepicker.init(today, nextYear.getTime()).withSelectedDate(today)
-                .inMode(CalendarPickerView.SelectionMode.RANGE)
-                .withSelectedDate(today);
+        final Calendar lastYear = Calendar.getInstance();
+        lastYear.add(Calendar.YEAR, - 10);
 
-        List<Date> dates = datepicker.getSelectedDates();// This line has error
-        Log.d("Date", dates.toString());
+        calendar = findViewById(R.id.calendar_view);
+        button = findViewById(R.id.get_selected_dates);
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
 
-        datepicker.setOnDateSelectedListener(new CalendarPickerView.OnDateSelectedListener() {
+        //calendar.deactivateDates(list);
+        ArrayList<Date> arrayList = new ArrayList<>();
+        try {
+            SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
+
+            String strdate = "22-4-2019";
+            String strdate2 = "26-4-2019";
+
+            Date newdate = dateformat.parse(strdate);
+            Date newdate2 = dateformat.parse(strdate2);
+            arrayList.add(newdate);
+            arrayList.add(newdate2);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        calendar.init(lastYear.getTime(), nextYear.getTime(), new SimpleDateFormat("MMMM, YYYY", Locale.getDefault())) //
+                .inMode(CalendarPickerView.SelectionMode.RANGE) //
+                // .withDeactivateDates(list)
+                .withSubTitles(getSubTitles())
+                .withHighlightedDates(arrayList);
+
+        calendar.scrollToDate(new Date());
+
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDateSelected(Date date) {
-                String selectedDate = DateFormat.getDateInstance(DateFormat.FULL).format(date);
-                Calendar selectedCalendar = Calendar.getInstance();
-                selectedCalendar.setTime(date);
-                /*String selectedDate = "" + selectedCalendar.get(Calendar.DAY_OF_MONTH)
-                        + " " + (selectedCalendar.get(Calendar.MONTH) + 1)
-                        + " " + selectedCalendar.get(Calendar.YEAR);*/
-                //Toast.makeText(CalenderActivity.this, selectedDate, Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onDateUnselected(Date date) {
-
+            public void onClick(View view) {
+                //List<String> dates = new ArrayList<String>();
+                //dates.add(calendar.getSelectedDates().toString());
+                Toast.makeText(CalenderActivity.this, calendar.getSelectedDates().toString(), Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    private ArrayList<SubTitle> getSubTitles() {
+        final ArrayList<SubTitle> subTitles = new ArrayList<>();
+        final Calendar tmrw = Calendar.getInstance();
+        tmrw.add(Calendar.DAY_OF_MONTH, 1);
+        subTitles.add(new SubTitle(tmrw.getTime(), "₹1000"));
+        return subTitles;
+    }
+
+
 }
