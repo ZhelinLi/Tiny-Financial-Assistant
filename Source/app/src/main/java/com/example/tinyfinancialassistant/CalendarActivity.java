da4759ebb333bbb0043ab645ed04b8d63d8090ba
zhyang98 <49853737+zhyang98@users.noreply.github.com>
2019-12-01 16:16:56 -0800

    date parsed

--- Source/app/src/main/java/com/example/tinyfinancialassistant/CalendarActivity.java
+++ Source/app/src/main/java/com/example/tinyfinancialassistant/CalendarActivity.java
@@ -1,6 +1,7 @@
 package com.example.tinyfinancialassistant;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -8,13 +9,14 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.savvi.rangedatepicker.CalendarPickerView;
-import com.savvi.rangedatepicker.SubTitle;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 public class CalendarActivity extends AppCompatActivity {
@@ -46,7 +48,7 @@ public class CalendarActivity extends AppCompatActivity {
         list.add(2);
 
         //calendar.deactivateDates(list);
-        ArrayList<Date> arrayList = new ArrayList<>();
+        final ArrayList<Date> arrayList = new ArrayList<>();
         try {
             SimpleDateFormat dateformat = new SimpleDateFormat("dd-MM-yyyy");
 
@@ -64,7 +66,7 @@ public class CalendarActivity extends AppCompatActivity {
         calendar.init(lastYear.getTime(), nextYear.getTime(), new SimpleDateFormat("MMMM, YYYY", Locale.getDefault())) //
                 .inMode(CalendarPickerView.SelectionMode.RANGE) //
                 // .withDeactivateDates(list)
-                .withSubTitles(getSubTitles())
+                //.withSubTitles(getSubTitles())
                 .withHighlightedDates(arrayList);
 
         calendar.scrollToDate(new Date());
@@ -73,20 +75,40 @@ public class CalendarActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //List<String> dates = new ArrayList<String>();
+                List<Date> primdates = calendar.getSelectedDates();
+                List<String> dates = new ArrayList<>();
+                for (Date date : primdates) {
+                    //String dateStr = String.valueOf(date);
+                    String dateStr = dateToString(date);
+                    dates.add(dateStr);
+                }
                 //dates.add(calendar.getSelectedDates().toString());
+                Log.d("date", String.valueOf(dates));
+                //Log.d("date2", String.valueOf(arrayList));
                 Toast.makeText(CalendarActivity.this, calendar.getSelectedDates().toString(), Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private ArrayList<SubTitle> getSubTitles() {
+    /*private ArrayList<SubTitle> getSubTitles() {
         final ArrayList<SubTitle> subTitles = new ArrayList<>();
         final Calendar tmrw = Calendar.getInstance();
         tmrw.add(Calendar.DAY_OF_MONTH, 1);
         subTitles.add(new SubTitle(tmrw.getTime(), "₹1000"));
         return subTitles;
-    }
+    }*/
+    public static String dateToString(Date date) {
+        String convertedDate = "";
 
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            convertedDate = dateFormat.format(date);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return convertedDate;
+    }
 
 }
